2c1280a0a79cead78312066a90a2b6bf70c3dd44
Kristopher Jafeth <43774719+kristopherjafeth@users.noreply.github.com>
2022-08-01 21:18:08 -0400

    ins

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -5,6 +5,7 @@ import SectionSubtitle from "./SectionSubtitle";
 import network from "../../public/images/Connected world.png";
 import Slider from "react-slick";
 import classes from "../../styles/testimonial.module.css";
+import Instagram from "./script"
 
 const Testimonial = () => {
   const settings = {
@@ -26,9 +27,9 @@ const Testimonial = () => {
           <Col lg="12" md="12">
             <SectionSubtitle subtitle="Redes" />
             <h4 className="mt-4 mb-5">Instagram</h4>
-           <script src="https://cdn.lightwidget.com/widgets/lightwidget.js" async />
-           <iframe src="//lightwidget.com/widgets/cdd12828e0de5d75a976f4cf215c495a.html" style={{ width: "100%", overflow: "hidden", }} scrolling="no" allowtransparency="true" className="lightwidget-widget" ></iframe>
-          </Col>
+            <Instagram />
+            <div className="elfsight-app-3b4a04a4-5b77-401d-b4a7-bcc4fe202869"></div>          
+            </Col>
         </Row>
       </Container>
     </section>
---
+++ components/UI/script.js
@@ -0,0 +1,13 @@
+import Script from 'next/script'
+
+function Instagram() {
+  return (
+    <div class="container">
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+
+
+    </div>
+  )
+}
+
+export default Instagram
