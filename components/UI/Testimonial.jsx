36d4bd0e8bee6e5b767be506f7f13aca3a0311b6
Kristopher Jafeth <43774719+kristopherjafeth@users.noreply.github.com>
2022-08-01 21:03:30 -0400

    Update Testimonial.jsx

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -27,7 +27,7 @@ const Testimonial = () => {
             <SectionSubtitle subtitle="Redes" />
             <h4 className="mt-4 mb-5">Instagram</h4>
            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-           <iframe src="//lightwidget.com/widgets/cdd12828e0de5d75a976f4cf215c495a.html" style={{ width: "100%", overflow: "hidden", }} scrolling="no" allowtransparency="true" class="lightwidget-widget" ></iframe>
+           <iframe src="//lightwidget.com/widgets/cdd12828e0de5d75a976f4cf215c495a.html" style={{ width: "100%", overflow: "hidden", }} scrolling="no" allowtransparency="true" className="lightwidget-widget" ></iframe>
           </Col>
         </Row>
       </Container>
